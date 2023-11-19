45561920c8d761d9fd369127aea549dd36ca0be8
Oleksandr Borys <barbaris.in@gmail.com>
2023-11-19 23:45:17 +0200

    Added `onMotionDetected` and `onMotionStopped` events to MotionSensor

--- src/devices/motion-sensor.ts
+++ src/devices/motion-sensor.ts
@@ -1,26 +1,54 @@
 import GenericMqttDevice from "./generic-mqtt-device";
+import timers from "../core/timers";
 const logger = require('../core/logger').logger('devices-motion-sensor');
 
 export default class MotionSensor extends GenericMqttDevice {
     public readonly type: string = 'motion-sensor';
-    // todo: what default value should be here? possibly null?
-    private occupancy: boolean = true;
+    // todo: what default value should be here? possibly null? better to initialize it in constructor from cached value
+    private occupancy: boolean | null = null;
 
-    public setOccupancy(occupancy: boolean): boolean {
-        const result = this.occupancy !== occupancy;
+    constructor(protected readonly id: string, protected name: string, protected object: any) {
+        super(id, name, object);
+
+        this.on('status_changed', (params: any) => {
+            if (undefined === params.occupancy) {
+                logger.error('Invalid motion sensor status', {name: this.getName(), params});
+                return;
+            }
+            this.setOccupancy(params.occupancy);
+        });
+    }
+
+    public setOccupancy(occupancy: boolean): void {
+        const changed = this.occupancy !== occupancy;
         this.occupancy = occupancy;
-        return result;
+
+        if (changed) {
+            if (occupancy) {
+                logger.debug('Motion detected', {deviceName: this.getName()});
+                this.emit('motion_detected', {})
+                timers.clearTimer(this.getName());
+            } else {
+                logger.debug('Motion stopped', {deviceName: this.getName()});
+                this.emit('motion_stopped', {});
+            }
+        }
+
+        return;
     }
 
-    public onStatusChanged(callback: Function): void {
-        this.on('status_changed', callback);
+    /**
+     * @internal
+     */
+    // protected onStatusChanged(callback: Function): void {
+    //     this.on('status_changed', callback);
+    // }
+
+    public onMotionDetected(callback: Function): void {
+        this.on('motion_detected', callback);
     }
 
-    // public onMotionDetected(callback: Function): void {
-    //     this.on('motion_detected', callback);
-    // }
-    //
-    // public onMotionStopped(callback: Function): void {
-    //     this.on('motion_stopped', callback);
-    // }
+    public onMotionStopped(callback: Function): void {
+        this.on('motion_stopped', callback);
+    }
 }
--- src/extensions/automation1/index.ts
+++ src/extensions/automation1/index.ts
@@ -16,35 +16,39 @@ class AutomationExtension extends Extension {
             const motionSensor = deviceManager.getDeviceByName(motionDeviceName);
 
             if (motionSensor instanceof MotionSensor) {
-                motionSensor.onStatusChanged((params: any) => {
-                    logger.debug('Moving alert', {motionDeviceName, params});
-                    if (undefined === params.occupancy) {
-                        logger.error('Invalid motion sensor status', {motionDeviceName, params});
-                        return;
-                    }
-
-                    // if occupancy just changed
-                    if (motionSensor.setOccupancy(params.occupancy)) {
-                        if (params.occupancy) {
-                            logger.debug('Motion detected', {motionDeviceName, params});
-                            timers.clearTimer(motionDeviceName);
-                            const light = deviceManager.getDeviceByName(lightDeviceName);
-                            if (light instanceof Bulb) {
-                                light.turnOn();
-                            }
-                        } else {
-                            logger.debug('Motion not detected', {motionDeviceName, params});
-                            const timer = setTimeout(() => {
-                                logger.debug('Light off', {motionDeviceName, params});
-                                const light = deviceManager.getDeviceByName(lightDeviceName);
-                                if (light instanceof Bulb) {
-                                    light.turnOff();
-                                }
-                            }, timeout * 1000);
-                            timers.refreshTimer(motionDeviceName, timer);
-                        }
+                motionSensor.onMotionDetected((params: any) => {
+                    logger.debug('Motion detected', {motionDeviceName, params});
+                    timers.clearTimer(motionDeviceName);
+                    const light = deviceManager.getDeviceByName(lightDeviceName);
+                    if (light instanceof Bulb) {
+                        light.turnOn();
                     }
                 });
+                motionSensor.onMotionStopped((params: any) => {
+                    logger.debug('Motion stopped', {motionDeviceName, params});
+                    const timer = setTimeout(() => {
+                        logger.debug('Light off', {motionDeviceName, params});
+                        const light = deviceManager.getDeviceByName(lightDeviceName);
+                        if (light instanceof Bulb) {
+                            light.turnOff();
+                        }
+                    }, timeout * 1000);
+                    timers.refreshTimer(motionDeviceName, timer);
+                });
+                // motionSensor.onStatusChanged((params: any) => {
+                //     logger.debug('Moving alert', {motionDeviceName, params});
+                //     if (undefined === params.occupancy) {
+                //         logger.error('Invalid motion sensor status', {motionDeviceName, params});
+                //         return;
+                //     }
+                //
+                //     // if occupancy just changed
+                //     if (motionSensor.setOccupancy(params.occupancy)) {
+                //         if (params.occupancy) {
+                //         } else {
+                //         }
+                //     }
+                // });
             }
         }
 
