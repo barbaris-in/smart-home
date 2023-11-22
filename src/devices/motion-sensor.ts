d5875a01514731bf73acf793cea2b3f93ce1e961
Oleksandr Borys <barbaris.in@gmail.com>
2023-11-22 13:17:49 +0200

    Refine motion sensor to track additional device parameters
    The motion sensor now tracks additional device parameters such as "battery" level and "illuminance". Changes were made on `motion-sensor.ts`, including new protected fields for these parameters and corresponding setter methods. If these parameters are changed, the old and new values are logged. Events have been added for changes in illuminance and battery level. This expanded tracking provides a more comprehensive overview of a device's status.

--- src/devices/motion-sensor.ts
+++ src/devices/motion-sensor.ts
@@ -1,20 +1,59 @@
 import GenericMqttDevice from "./generic-mqtt-device";
+
 const logger = require('../core/logger').logger('devices-motion-sensor');
 
 export default class MotionSensor extends GenericMqttDevice {
     public readonly type: string = 'motion-sensor';
     // todo: what default value should be here? possibly null? better to initialize it in constructor from cached value
-    private occupancy: boolean | null = null;
+    protected occupancy: boolean | null = null;
+    protected illuminance: number | null = null;
+    protected battery: number | null = null;
+    // protected deviceTemperature: number | null = null;
+    // protected power_outage_count: number | null = null;
+    // protected voltage: number | null = null;
 
     constructor(protected readonly id: string, protected name: string) {
         super(id, name);
 
+        /**
+         * Detect any device changes. Example:
+         * ```json
+         * {
+         *     "battery":100,
+         *     "device_temperature":24,
+         *     "illuminance":8,
+         *     "illuminance_lux":8,
+         *     "linkquality":255,
+         *     "occupancy":true,
+         *     "power_outage_count":93,
+         *     "voltage":3025
+         * }
+         * ```
+         */
         this.on('status_changed', (params: any) => {
             if (undefined === params.occupancy) {
-                logger.error('Invalid motion sensor status', {name: this.getName(), params});
-                return;
+                logger.error('No occupancy in motion sensor status', {name: this.getName(), params});
+            } else {
+                this.setOccupancy(params.occupancy);
             }
-            this.setOccupancy(params.occupancy);
+
+            if (undefined === params.illuminance) {
+                logger.error('No illuminance in motion sensor status', {name: this.getName(), params});
+            } else {
+                this.setIlluminance(params.illuminance);
+            }
+
+            if (undefined === params.battery) {
+                logger.error('No battery in motion sensor status', {name: this.getName(), params});
+            } else {
+                this.setBattery(params.battery);
+            }
+
+            // this.setDeviceTemperature(params.device_temperature);
+            // this.setIlluminanceLux(params.illuminance_lux);
+            // this.setLinkQuality(params.linkquality);
+            // this.setPowerOutageCount(params.power_outage_count);
+            // this.setVoltage(params.voltage);
         });
     }
 
@@ -35,6 +74,40 @@ export default class MotionSensor extends GenericMqttDevice {
         return;
     }
 
+    protected setBattery(newValue: number): void {
+        const oldValue = this.battery;
+        const changed = oldValue !== newValue;
+        this.battery = newValue;
+
+        if (oldValue !== null && changed) {
+            logger.debug('Battery level changed', {deviceName: this.getName(), oldValue, newValue});
+            this.emit('battery_level_changed', {oldValue, newValue});
+        }
+    }
+
+    protected setIlluminance(newValue: number): void {
+        const oldValue = this.battery;
+        const changed = oldValue !== newValue;
+        this.illuminance = newValue;
+
+        if (oldValue !== null && changed) {
+            logger.debug('Illuminance changed', {deviceName: this.getName(), oldValue, newValue});
+            this.emit('illuminance_changed', {oldValue, newValue});
+        }
+    }
+
+    // protected setLinkQuality(linkquality: any) {
+    //
+    // }
+    //
+    // protected setPowerOutageCount(power_outage_count: any) {
+    //
+    // }
+    //
+    // protected setVoltage(voltage: any) {
+    //
+    // }
+
     /**
      * @internal
      */
@@ -48,5 +121,13 @@ export default class MotionSensor extends GenericMqttDevice {
 
     public onMotionStopped(callback: Function): void {
         this.on('motion_stopped', callback);
+    }
+
+    public onIlluminanceChanged(callback: Function): void {
+        this.on('illuminance_changed', callback);
+    }
+
+    public onBatteryLevelChanged(callback: Function): void {
+        this.on('battery_level_changed', callback);
     }
 }
