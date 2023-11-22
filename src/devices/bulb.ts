865bab4a8c6bc6e90f2694fbe24e7ff90b1be302
Oleksandr Borys <barbaris.in@gmail.com>
2023-11-22 14:19:57 +0200

    Expand Bulb class functionality and track additional parameters
    The Bulb class now has additional parameters tracked (state, brightness, color temperature) and corresponding setter methods (i.e., setBrightness(), setColorTemperature()). An event listener has been added on 'status_changed' to update these parameters as changes occur. Improved the 'turnOn()' and 'turnOff()' methods by creating a 'sendCommand' function to clear repetitive tasks. These changes will provide a thorough overview of each bulb's current settings and improve code maintainability.

--- src/devices/bulb.ts
+++ src/devices/bulb.ts
@@ -3,16 +3,59 @@ import timers from "../core/timers";
 
 export default class Bulb extends GenericDevice {
     public readonly type: string = 'bulb';
+    protected state: string | null = null;
+    protected brightness: number | null = null;
+    protected color_temp: number | null = null;
+
+    constructor(protected readonly id: string, protected name: string) {
+        super(id, name);
+
+        /**
+         * Detect any device changes. Example:
+         * ```
+         * {
+         *   "brightness": 254,
+         *   "color_mode": "color_temp",
+         *   "color_temp": 187,
+         *   "linkquality": 156,
+         *   "state": "ON",
+         *   "update": {
+         *     "installed_version": 34,
+         *     "latest_version": 34,
+         *     "state": "idle"
+         *   },
+         *   "update_available": false
+         * }
+         * ```
+         * __Note:__ Only some of the properties can be received, depending on the last command. Example:
+         * ```
+         * {
+         *   "brightness": 254
+         * }
+         * ```
+         */
+        this.on('status_changed', (params: any) => {
+            if (undefined !== params.state) {
+                this.state = params.state;
+            }
+
+            if (undefined !== params.brightness) {
+                this.brightness = params.brightness;
+            }
+
+            if (undefined !== params.color_temp) {
+                this.color_temp = params.color_temp;
+            }
+        });
+    }
 
     public turnOn(): void {
-        const mqtt = require("../extensions/mqtt").default;
-        mqtt.client.publish(`zigbee2mqtt/${this.name}/set`, '{"state":"ON"}');
+        this.sendCommand({state: 'ON'});
         timers.clearTimer(this.getName());
     }
 
     public turnOff(): void {
-        const mqtt = require("../extensions/mqtt").default;
-        mqtt.client.publish(`zigbee2mqtt/${this.name}/set`, '{"state":"OFF"}');
+        this.sendCommand({state: 'OFF'});
         timers.clearTimer(this.getName());
     }
 
@@ -26,5 +69,30 @@ export default class Bulb extends GenericDevice {
     public toggle(): void {
         // todo: implement bulb toggle
         throw new Error('Method not implemented.');
+    }
+
+    /**
+     * value_max: 254
+     * value_min: 0
+     * @param brightness
+     */
+    public setBrightness(brightness: number): void {
+        this.sendCommand({brightness: brightness});
+    }
+
+    /**
+     * value_max: 370
+     * value_min: 153
+     * @param colorTemperature
+     */
+    public setColorTemperature(colorTemperature: number): void {
+        this.sendCommand({color_temp: colorTemperature});
+    }
+
+    protected sendCommand(command: {}): void {
+        const commandString = JSON.stringify(command);
+        const mqtt = require("../extensions/mqtt").default;
+        mqtt.client.publish(`zigbee2mqtt/${this.name}/set`, commandString);
+        timers.clearTimer(this.getName());
     }
 }
