1a26ddbf110fa140461743784d04e7fe10b457d5
Oleksandr Borys <barbaris.in@gmail.com>
2023-11-22 14:46:04 +0200

    Change Cube's rotation effects on Bulb device
    Enhanced the Cube's interaction with the Bulb device on its rotations. The left and right rotations now adjust (decrease and increase respectively) the brightness and color temperature of the Bulb. If the cube is flipped 90 degree, the mode switches between brightness and color temperature. A logger error is added when no rotation angle is detected. Device is fetched in the beginning to prevent multiple fetches and log an error if not found.

--- src/extensions/zcube/index.ts
+++ src/extensions/zcube/index.ts
@@ -16,26 +16,35 @@ class CubeAutomation extends Extension {
             logger.error('Cube not found');
             return;
         }
+        const bulb: Device | null = deviceManager.getDeviceByName('Office Desk Light');
+        if (!(bulb instanceof Bulb)) {
+            logger.error('Bulb not found');
+            return;
+        }
 
         if (cube instanceof Cube) {
-            cube.onRotateLeft((params: any) => {
-                const bulb: Device | null = deviceManager.getDeviceByName('Office Desk Light');
-                if (bulb instanceof Bulb) {
-                    bulb.turnOff();
+            let brightnessMode = true;
+
+            const rotationCallback = (params: any) => {
+                console.log('=======================================================================================', 'brightnessMode', brightnessMode);
+                if (!params.angle) {
+                    logger.error('No angle', params);
+                    return;
                 }
-            });
-            cube.onRotateRight((params: any) => {
-                const bulb: Device | null = deviceManager.getDeviceByName('Office Desk Light');
-                if (bulb instanceof Bulb) {
-                    bulb.turnOn();
+                const diff = Math.round(params.angle);
+                if (brightnessMode) {
+                    bulb.setBrightness((bulb.getBrightness() || 0) + diff);
+                } else {
+                    bulb.setColorTemperature((bulb.getColorTemperature() || 0) + diff);
                 }
-            });
-            // cube.on('slide', (params: any) => {
-            //     const bulb: Device | null = devices.getDeviceByName('Office Desk Light');
-            //     if (bulb instanceof Bulb) {
-            //         bulb.turnOff();
-            //     }
-            // });
+            };
+
+            cube.onRotateRight(rotationCallback);
+            cube.onRotateLeft(rotationCallback);
+            cube.onFlip90(() => {
+                brightnessMode = !brightnessMode;
+                console.log('=======================================================================================', 'brightnessMode', brightnessMode);
+            })
         }
     }
 }
