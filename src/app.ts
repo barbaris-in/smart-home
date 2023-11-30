939dd2c63a576940c56ae758d126090be7544775
Oleksandr Borys <barbaris.in@gmail.com>
2023-11-30 11:03:33 +0200

    Move ASCII art console logs from app.ts to index.js
    Removed multi-line ASCII text console logs from the top of the source file (app.ts) and moved them to the application entry point (index.js). T

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,3 @@
-console.log('███████╗███╗   ███╗ █████╗ ██████╗ ████████╗ ██████╗ ███╗   ███╗███████╗');
-console.log('██╔════╝████╗ ████║██╔══██╗██╔══██╗╚══██╔══╝██╔═══██╗████╗ ████║██╔════╝');
-console.log('███████╗██╔████╔██║███████║██████╔╝   ██║   ██║   ██║██╔████╔██║█████╗');
-console.log('╚════██║██║╚██╔╝██║██╔══██║██╔══██╗   ██║   ██║   ██║██║╚██╔╝██║██╔══╝');
-console.log('███████║██║ ╚═╝ ██║██║  ██║██║  ██║   ██║   ╚██████╔╝██║ ╚═╝ ██║███████╗');
-console.log('╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝     ╚═╝╚══════╝');
-
 import deviceClassRegistry from "./core/device-class-registry";
 import deviceManager from "./core/device-manager";
 import extensionsLoader from "./core/extensions-loader";
