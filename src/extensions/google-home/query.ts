3348f4f17e6350537ce1b16f59c9a1fdbdb639b0
Oleksandr Borys <barbaris.in@gmail.com>
2024-01-13 17:19:18 +0200

    Handle lost devices for google

--- src/extensions/google-home/query.ts
+++ src/extensions/google-home/query.ts
@@ -13,50 +13,58 @@ export default class Query {
 
         for (const requestDevice of intent.payload.devices) {
             const deviceId = requestDevice.id;
-            const device: Device = deviceManager.getDevice(deviceId);
-
             const responseDevice: any = {};
-            if (device.supports(OnOff)) {
-                responseDevice.on = OnOff(device).getOnOff();
+            try {
+                const device: Device = deviceManager.getDevice(deviceId);
+                if (device.supports(OnOff)) {
+                    responseDevice.on = OnOff(device).getOnOff();
+                }
+                if (device.supports(Brightness)) {
+                    responseDevice.brightness = Brightness(device).getBrightnessPercentage();
+                }
+                if (device.supports(ColorTemperature)) {
+                    responseDevice.color = {temperatureK: ColorTemperature(device).getColorTemperatureKelvin()};
+                }
+                if (device.supports(SecuritySystem)) {
+                    responseDevice.isArmed = SecuritySystem(device).isArmed();
+                    responseDevice.currentArmLevel = SecuritySystem(device).getCurrentArmLevel();
+                    responseDevice.currentStatusReport = [];
+                    // responseDevices.currentStatusReport = [
+                    //     {
+                    //         "blocking": false,
+                    //         "deviceTarget": "alarm_1",
+                    //         "priority": 0,
+                    //         "statusCode": "lowBattery"
+                    //     },
+                    //     {
+                    //         "blocking": false,
+                    //         "deviceTarget": "front_window_1",
+                    //         "priority": 1,
+                    //         "statusCode": "deviceOpen"
+                    //     },
+                    //     {
+                    //         "blocking": false,
+                    //         "deviceTarget": "back_window_2",
+                    //         "priority": 1,
+                    //         "statusCode": "deviceOpen"
+                    //     },
+                    //     {
+                    //         "blocking": true,
+                    //         "deviceTarget": "alarm_2",
+                    //         "priority": 0,
+                    //         "statusCode": "needsSoftwareUpdate"
+                    //     }
+                    // ];
+                }
+                responseDevice.online = true; // todo: check if device is online
+                responseDevices[deviceId] = responseDevice;
+            } catch (e: any) {
+                responseDevice.online = false;
+                responseDevice.status = 'ERROR';
+                console.log(e);
+                responseDevice.errorCode = e.message;
+                logger.error('Device not found', {deviceId});
             }
-            if (device.supports(Brightness)) {
-                responseDevice.brightness = Brightness(device).getBrightnessPercentage();
-            }
-            if (device.supports(ColorTemperature)) {
-                responseDevice.color = {temperatureK: ColorTemperature(device).getColorTemperatureKelvin()};
-            }
-            if (device.supports(SecuritySystem)) {
-                responseDevice.isArmed = SecuritySystem(device).isArmed();
-                responseDevice.currentArmLevel = SecuritySystem(device).getCurrentArmLevel();
-                responseDevice.currentStatusReport = [];
-                // responseDevices.currentStatusReport = [
-                //     {
-                //         "blocking": false,
-                //         "deviceTarget": "alarm_1",
-                //         "priority": 0,
-                //         "statusCode": "lowBattery"
-                //     },
-                //     {
-                //         "blocking": false,
-                //         "deviceTarget": "front_window_1",
-                //         "priority": 1,
-                //         "statusCode": "deviceOpen"
-                //     },
-                //     {
-                //         "blocking": false,
-                //         "deviceTarget": "back_window_2",
-                //         "priority": 1,
-                //         "statusCode": "deviceOpen"
-                //     },
-                //     {
-                //         "blocking": true,
-                //         "deviceTarget": "alarm_2",
-                //         "priority": 0,
-                //         "statusCode": "needsSoftwareUpdate"
-                //     }
-                // ];
-            }
-            responseDevice.online = true; // todo: check if device is online
             responseDevices[deviceId] = responseDevice;
         }
         return {
