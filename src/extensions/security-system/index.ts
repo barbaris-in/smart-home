b0bc73d71e0eca3be81cb72702083bb1c0d15825
Oleksandr Borys <barbaris.in@gmail.com>
2024-03-06 12:14:50 +0200

    Minor fix

--- src/extensions/security-system/index.ts
+++
@@ -1,5 +0,0 @@
-import Extension from "../../core/abstract-extension";
-import {Device} from "../../core/device";
-import deviceManager from "../../core/device-manager";
-import {SecuritySystem, SecuritySystemTrait} from "./SecuritySystem";
-
